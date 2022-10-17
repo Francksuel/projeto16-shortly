49afdc93243baa50196a1dbe08a114657f8f99a6
Francksuel <francksuelbarbosa@gmail.com>
2022-10-17 15:26:52 -0300

    Project finished

--- src/controllers/ranking.controller.js
+++ src/controllers/ranking.controller.js
@@ -4,8 +4,14 @@ const getRanking = async (req, res) => {
 	try {
 		const ranking = await connection.query(
 			`SELECT u.id,u.name,COUNT(ur.*) AS "linksCount", COALESCE(SUM(ur."visitCount"),0) AS "visitCount"  
-            FROM users u LEFT JOIN urls ur ON u.id = ur."userId" GROUP BY u.id ORDER BY "visitCount" DESC LIMIT 10;`
+            FROM users u LEFT JOIN urls ur ON u.id = ur."userId" GROUP BY u.id ORDER BY "visitCount" DESC,"linksCount" DESC;`
 		);
+		if (ranking.rows.length !== 0) {
+			ranking.rows.forEach((user) => {
+				user.linksCount = Number(user.linksCount);
+				user.visitCount = Number(user.visitCount);
+			});
+		}
 		res.send(ranking.rows);
 	} catch {
 		res.sendStatus(500);
--- src/middlewares/urlSchema.middleware.js
+++ src/middlewares/urlSchema.middleware.js
@@ -2,21 +2,27 @@ import joi from "joi";
 import { stripHtml } from "string-strip-html";
 
 const urlSchema = joi.object({
-	url: joi.string().uri().required()	
+	url: joi
+		.string()
+		.pattern(
+			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[-a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.?&//=]*)/
+		)
+		.required(),
 });
 
 const urlValidationSchema = (req, res, next) => {
-	const link = req.body;	
+	const link = req.body;
 	if (link.url) {
 		link.url = stripHtml(link.url).result.trim();
-		
 	}
 	const urlValidation = urlSchema.validate(link, { abortEarly: false });
 	if (urlValidation.error) {
-		const errors = (urlValidation.error.details.map((error) => error.message)).join(" & ");
+		const errors = urlValidation.error.details
+			.map((error) => error.message)
+			.join(" & ");
 		return res.status(422).send(errors);
 	}
 	res.locals.url = link.url;
 	next();
 };
-export {urlValidationSchema};
+export { urlValidationSchema };
